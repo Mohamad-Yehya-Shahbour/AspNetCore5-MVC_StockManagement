deba2a17049d50681baeef9240f1b8e8f6cf2613
Mohamad Yehya <mohamad.yehya.shahbur@gmail.com>
2022-09-04 22:06:25 +0300

    add subItemDb function

---
+++ StockManagement/Data/SubItemDb.cs
@@ -0,0 +1,18 @@
+using StockManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StockManagement.Data
+{
+    public class SubItemDb
+    {
+        private readonly StockDbContext _context;
+        public SubItemDb(StockDbContext context)
+        {
+            _context = context;
+        }
+
+    }
+}
